1e7450bc06fd2c0a617da21316f9d39663febf7d
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-01-30 22:39:06 -0300

    Criado componente item

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+
+import Item from './Item';
 
 export default class ListItems extends Component {
   
-  constructor(props) {
-    super(props);
-    console.log('Contruindo a aplicação.');
-  }
-
-  componentWillMount() {
-    console.log('Fazer alguma coisa antes de renderizar.');
-  }
-
-  componentDidMount() {
-    console.log('Fazer alguma coisa depois da renderização');
-  }
-
   render() {
     console.log('Objeto é renderizado.');
     return (
-        <Text>Teste de importação de componente</Text>
+      <View>
+          <Item />
+          <Item />
+          <Item />
+      </View>      
     );
   }
 }

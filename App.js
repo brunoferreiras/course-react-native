c9f433f2cddfac3fc6550c557c7a527faadfa8be
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-01-30 22:20:06 -0300

    Init project App4 - course Udemy

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { } from 'react-native';
+
+import ListItems from './src/components/ListItems';
+
+export default class App extends Component {
+  render() {
+    return (
+      <ListItems />
+    );
+  }
+}

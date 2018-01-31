408c0130614c9af4c272757e97c0e201af087495
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-01-31 07:51:26 -0300

    Adicionado informações no componente item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 export default class Item extends Component {
   
   render() {
     return (
-      <Text>Detalhes do item</Text>      
+      <View>
+        <Image style={{ height: 100, width: 100 }} source={{ uri: this.props.data.foto }} />
+        <Text>{this.props.data.titulo}</Text>
+        <Text>{this.props.data.valor}</Text>
+        <Text>{this.props.data.local_anuncio}</Text>
+        <Text>{this.props.data.data_publicacao}</Text>
+      </View>      
     );
   }
 }
--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import Item from './Item';
 
 export default class ListItems extends Component {
   
+  constructor(props) {
+    super(props);
+
+    this.state = { listItems: [] };
+  }
+
   componentWillMount() {
     axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-      .then(response => { console.log(response); })
+      .then(response => { this.setState({ listItems: response.data }); })
       .catch(() => { console.log('Erro ao recuperar os dados'); });
   }
 
   render() {
-    console.log('Objeto é renderizado.');
     return (
-      <View>
-          <Item />
-          <Item />
-          <Item />
-      </View>      
+      <ScrollView>
+          { this.state.listItems.map(item => (<Item key={item.titulo} data={item} />)) }
+      </ScrollView>      
     );
   }
 }

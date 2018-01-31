19f52d9f1e33c060e923f5810782ca71d44753a1
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-01-31 11:30:31 -0300

    Estiliza ClientScene

--- App5/src/components/ClientScene.js
+++ App5/src/components/ClientScene.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StatusBar, View, Image, Text } from 'react-native';
+import { StatusBar, View, Image, Text, StyleSheet } from 'react-native';
 
 import BarNavigation from './BarNavigation';
 
@@ -16,14 +16,43 @@ export default class ClientScene extends Component {
         </StatusBar>
 
         <BarNavigation />
+
+        <View style={styles.detailsClients}>
+          <Image source={detailsClients} />
+          <Text style={styles.title}>Nossos Clientes</Text>
+        </View>
         
-        <Image source={detailsClients} />
-        <Text>Nossos clientes</Text>
-        <Image source={cliente1} />
-        <Text>cliente 1</Text>
-        <Image source={cliente2} />
-        <Text>cliente 2</Text>
+        <View style={styles.clientGroup}>
+          <Image source={cliente1} />
+          <Text style={styles.textGroup}>cliente 1</Text>
+        </View>
+
+        <View style={styles.clientGroup}>
+          <Image source={cliente2} />
+          <Text style={styles.textGroup}>cliente 2</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  detailsClients: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 30,
+    color: '#B9C941',
+    marginLeft: 10
+  },
+  clientGroup: {
+    marginTop: 10,
+    padding: 20
+  },
+  textGroup: {
+    fontSize: 18,
+    marginLeft: 20
+  }
+});

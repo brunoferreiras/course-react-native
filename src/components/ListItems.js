d1f04e4c2a4596d46a238882f5fa4ea978c89366
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-01-30 22:58:10 -0300

    Adicionado requisição get com axios

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-
+import axios from 'axios';
 import Item from './Item';
 
 export default class ListItems extends Component {
   
+  componentWillMount() {
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then(response => { console.log(response); })
+      .catch(() => { console.log('Erro ao recuperar os dados'); });
+  }
+
   render() {
     console.log('Objeto é renderizado.');
     return (

bb7f5f6510aa5abd155cd2e4c6c56b92d6777b63
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-01-30 22:34:14 -0300

    Adicionado métodos do ciclo de vida de um componente

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 export default class ListItems extends Component {
+  
+  constructor(props) {
+    super(props);
+    console.log('Contruindo a aplicação.');
+  }
+
+  componentWillMount() {
+    console.log('Fazer alguma coisa antes de renderizar.');
+  }
+
+  componentDidMount() {
+    console.log('Fazer alguma coisa depois da renderização');
+  }
+
   render() {
+    console.log('Objeto é renderizado.');
     return (
         <Text>Teste de importação de componente</Text>
     );

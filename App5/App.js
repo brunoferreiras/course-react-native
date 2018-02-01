db57c59d3ccde0303bb911085f46545f89e53db9
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-01-31 21:56:09 -0300

    Importa navigator

--- App5/App.js
+++ App5/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import {
-  View
-} from 'react-native';
+import { View } from 'react-native';
 
+import { Navigator } from 'react-native-deprecated-custom-components';
+
+import MainScene from './src/components/MainScene';
 import ClientScene from './src/components/ClientScene';
 
 export default class App extends Component {
   render() {
     return (
-      <ClientScene />
+      <Navigator
+        initialRoute={{ id: 'a' }}
+        renderScene={(route, navigator) => {
+          if (route.id === 'a') {
+            return (<MainScene />);
+          }
+
+          if (route.id === 'b') {
+            return (<ClientScene />);
+          }
+        }}
+      />
     );
   }
 }

3b6a0cccdb20ee2a17df2cb227540f3c51cdf652
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-02-20 09:54:58 -0300

    Cria componente visor

--- App7/src/components/Result.js
+++ App7/src/components/Result.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Visor from './Visor';
 
 const Result = props => (
-  <View>
-    <Text>Resultado</Text>
-  </View>
+  <Visor />
 );
 
 export { Result };
---
+++ App7/src/components/Visor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+export default props => (
+  <View>
+    <TextInput 
+      style={styles.input}
+      placeholder='Resultado'
+      editable={false}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 30,
+    height: 100,
+    textAlign: 'center'
+  }
+});

24578f9e442b20d0156f97aacac446e1c96842b0
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-02-20 10:20:42 -0300

    Adiciona botão para calcular

--- App7/src/components/Command.js
+++ App7/src/components/Command.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button } from 'react-native';
 
 export default props => (
-  <Text>Comando</Text>
+  <Button title='Calcular' />
 );

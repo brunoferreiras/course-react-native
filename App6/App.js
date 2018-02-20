7ec7df5d67a4a9cb2fb983150a6b04c51697bb4e
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-02-19 22:32:48 -0300

    Implementa react router flux

--- App6/App.js
+++ App6/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import {
-  Platform
-} from 'react-native';
+import { } from 'react-native';
+import { Router, Scene, Stack } from 'react-native-router-flux';
+import Main from './src/components/Main';
+import OthersGames from './src/components/OthersGames';
+import AboutGame from './src/components/AboutGame';
 
 export default class App extends Component {
   render() {
     return (
-      
+      <Router>
+        <Stack key='root'>
+          <Scene key='main' component={Main} initial title='Cara ou coroa' />
+          <Scene key='aboutGame' component={AboutGame} />
+          <Scene key='othersGames' component={OthersGames} />
+        </Stack>
+      </Router>
     );
   }
 }

abc84be06c2601b06e7d2089f776d345f9ae5326
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-02-20 00:09:40 -0300

    Criado lógica do aplicativo

--- App6/src/components/Result.js
+++ App6/src/components/Result.js
@@ -5,14 +5,40 @@ const cara = require('../imgs/moeda_cara.png');
 const coroa = require('../imgs/moeda_coroa.png');
 
 export default class Result extends Component {
-    render() {
-        return (
-          <View style={styles.result}>
-            <Image source={cara} />
-            <Image source={coroa} />
-          </View>
-        );
+  constructor(props) {
+    super(props);
+
+    this.state = { result: '' };
+  }
+
+  componentWillMount() {
+    const numberRandom = Math.floor(Math.random() * 2);
+    let caraOrCoroa = '';
+
+    if (numberRandom === 0) {
+      caraOrCoroa = 'cara';
+    } else {
+      caraOrCoroa = 'coroa';
     }
+
+    this.setState({ result: caraOrCoroa });
+  }
+
+  render() {
+    if (this.state.result === 'cara') {
+      return (
+        <View style={styles.result}>
+          <Image source={cara} />
+        </View>
+      );
+    } 
+    
+    return (
+      <View style={styles.result}>
+        <Image source={coroa} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
